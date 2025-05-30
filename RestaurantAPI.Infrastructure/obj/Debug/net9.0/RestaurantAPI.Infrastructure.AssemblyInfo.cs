a451ea158ff612eea9ed04b7318b1a736fafdacd
Kacper Furmanczyk <k.furmanczykq@gmail.com>
2025-05-30 17:29:55 +0200

    code cleanup

--- RestaurantAPI.Infrastructure/obj/Debug/net9.0/RestaurantAPI.Infrastructure.AssemblyInfo.cs
+++ RestaurantAPI.Infrastructure/obj/Debug/net9.0/RestaurantAPI.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantAPI.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a091d23ce53baab35dc87343fb508e3cf954799")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb34db6dbff1ef0013dac5567992f0ad4786e6ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantAPI.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantAPI.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
